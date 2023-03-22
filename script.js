0614a995571353ca44d273858360d4770564e7f9
theisolatedmonk <96rahulseth@gmail.com>
2023-03-22 23:11:38 +0530

    updated commit

--- script.js
+++ script.js
@@ -237,3 +237,50 @@ let n = '1' + 1;
 n = n - 1;
 console.log(n);
 */
+
+/*
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('you should get a job!');
+}
+
+
+let height = 0;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFIND')
+}
+*/
+
+const age = '18';
+if (age === 18) console.log('You just become an adult:D');
+
+if (age == 18) console.log('You just become an adult:D');
+
+const favourite = prompt("What's your favourite number?");
+console.log(favourite);
+console.log(typeof favourite);
+23
+
+if (favourite === 23) {
+    console.log('Cool! 23 is an amazing number!')
+
+} else if (favourite == 7) {
+    console.log('7 is also a cool number')
+
+} else if (favourite == 9) {
+    console.log('9 is also a cool number')
+} else {
+    console.log('Number is not 23 or 7 or 9')
+}
+
+if (favourite != 23) console.log('Why not 23?');
